4b04f0044a5e715149af8e057625d3ad08bb5d9e
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 19:01:32 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+    render() {
+        return (
+            <div>
+                <input onKeyUp={(e) => {
+                    console.log("Entering password...")
+                }} type="password" />
+            </div>
+        )
+    }
+} 
